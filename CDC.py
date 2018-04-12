5078fc7d2293a9788f248a768eceed51927c44dc
rwjlee <lee.rwj@gmail.com>
2018-04-12 17:19:41 +0800

    works now

--- CDC.py
+++ CDC.py
@@ -2,6 +2,8 @@ import json, requests
 from pprint import pprint
 
 from base import DbManager
+from congress_entity import Member, Congress, Committee, SubCommittee
+
 
 API_BASE = "https://api.propublica.org/congress/v1/"
 API_KEY = "CDIz4elnUMmkTmHt6rZnNpJTYBElWYbDoG1tAd4o"
@@ -20,22 +22,76 @@ class CDC():
 
     def get_entity(self, obj, url):
         json_data= self.get_data(url)
-        print(url)
-                
-        pprint(json_data)
-
         entity = obj()
-        entity.parse_json(json_data)
+        
+        try:
+            entity.parse_json(json_data)
+        except:
+            entity=None
+            print("bad link: {}".format(url))
+            return entity
 
         results = self.db_Manager.open().query(obj).filter(obj.url == url).all()
-        print(results)
         if len(results)==0:
-            print("===================no results")
             self.db_Manager.save(entity)
+            print("new add: {}".format(entity.url))
         else:
-            print("should be here")
+            print("already added: {}".format(entity.url))
 
-        print(entity.name)
         return entity
 
-    
+    def populate_committee(self, congress, chamber):
+        
+        chamber = chamber.lower()
+        url = API_BASE + "{}/{}/committees.json".format(congress, chamber)
+
+        json_data = self.get_data(url)
+
+        try:
+            results = json_data['results'][0]
+        except:
+            print("Bad Link")
+            return "Bad Link"
+
+        committee_list = [c['api_uri'] for c in results['committees']]
+        subcommittee_list = [c['subcommittees'] for c in results['committees'] if c.get('subcommittees')]
+        sc_url_list = [sc for sc in subcommittee_list if len(sc)>0]
+
+        for sc_url in sc_url_list:
+            for sc in sc_url:
+                pprint(sc)
+                self.get_entity(SubCommittee, sc['api_uri'])
+
+        for com_url in committee_list:
+            self.get_entity(Committee, com_url)
+
+
+        return "Good Link"
+
+    def populate_members(self, congress, chamber):
+        
+        chamber=chamber.lower()
+        url = API_BASE + "{}/{}/members.json".format(congress, chamber)
+
+        json_data = self.get_data(url)
+
+        try:
+            results = json_data['results'][0]
+        except:
+            return "Bad Link"
+
+        member_list = [m['id'] for m in results['members']]
+
+        for mid in member_list:
+            member_url = API_BASE + "members/{}.json".format(mid)
+            self.get_entity(Member, member_url)
+        
+        return "Good Link"
+
+    def populate_house(self, congress):
+        url = API_BASE + "{}/{}/members.json".format(congress, "house")
+        self.get_entity(Congress, url)
+
+    def populate_senate(self, congress):
+        url = API_BASE + "{}/{}/members.json".format(congress, "senate")
+        self.get_entity(Congress, url)
--- client.py
+++ client.py
@@ -4,56 +4,17 @@ from pprint import pprint
 from CDC import CDC
 from congress_entity import Member, Congress, Committee, SubCommittee
 
-# entity_tuple = [(Member, "http://data.coding.kitchen/api/companies/1"), 
-#     (Club, "http://data.coding.kitchen/api/clubs/1"),
-#     (Person, "http://data.coding.kitchen/api/people/1"),
-#     (League, "http://data.coding.kitchen/api/leagues/"),
-#     (City, "http://data.coding.kitchen/api/cities/1"),
-#     (Department, "http://data.coding.kitchen/api/departments/1"),
-#     (State,"http://data.coding.kitchen/api/states/"),
-#     (Exchange, "http://data.coding.kitchen/api/exchanges/")]
-
-# for value in entity_tuple:
-#     obj, next_url = value
-#     dck.populate_table(obj, next_url)
-
-API_BASE = "https://api.propublica.org/congress/v1/"
-
-def get_senate(chamber):
-    if 80 <= chamber and chamber <= 115:
-        return get_data(API_BASE + ENDPOINT_SENATE.format(chamber)) 
-    else:
-        return None
-
-def get_house(chamber):
-    if 102 <= chamber and chamber <= 115:
-        return get_data(API_BASE + ENDPOINT_HOUSE.format(chamber))
-    else:
-        return None
-
-def get_members(chamber, branch):
-    pass
-
-
-
-
-# senate=get_senate(115)
-# print(len(senate['results']))
-
 cdc = CDC()
 
-# senator_url="https://api.propublica.org/congress/v1/members/C001095.json"
-# cdc.get_entity(Member, senator_url)
+for i in range(110, 116):
+    cdc.populate_committee(i, "senate")
+    cdc.populate_committee(i, "house")
+    cdc.populate_committee(i, "joint")
 
-# subcommittee_url = "https://api.propublica.org/congress/v1/115/senate/committees/SSAS/subcommittees/SSAS13.json"
-# cdc.get_entity(SubCommittee, subcommittee_url)
+for i in range(80, 116):
+    cdc.populate_senate(i)
+    cdc.populate_members(i, "senate")
 
-#committee_url = "https://api.propublica.org/congress/v1/115/senate/committees/SSAS.json"
-#cdc.get_entity(Committee, committee_url)
-
-# senate_url = "https://api.propublica.org/congress/v1/115/senate/members.json"
-# cdc.get_entity(Congress, senate_url)
-
-results=cdc.get_data("https://api.propublica.org/congress/v1/115/joint/committees.json")
-
-pprint(results)
+for i in range(102, 116):
+    cdc.populate_house(i)
+    cdc.populate_members(i, "house")
